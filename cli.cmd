6f578460d358dcf115344edbdc0728b0dfbaafd1
Dashby7 <abid.foued@gmail.com>
2021-01-01 15:24:41 +0100

    script cli de configuration des outils

---
+++ cli.cmd
@@ -0,0 +1,11 @@
+@echo off
+
+set JAVA_HOME=D:\dev\jdk\jdk-15.0.1
+set MAVEN_HOME=D:\dev\tools\apache-maven-3.6.3
+set PATH=%JAVA_HOME%\bin;%MAVEN_HOME%\bin;%PATH%
+
+cd D:\dev\projets\annoncesimmo\sources
+
+start cmd
+
+
